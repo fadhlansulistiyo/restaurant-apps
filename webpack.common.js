bf21e5d9b38fe7d4be6cca547af5666256fe810f
Fadhlan Sulistiyo Hidayat <fadhlansulistiyo@gmail.com>
2025-01-18 05:45:37 +0700

    chore: add code splitting

--- webpack.common.js
+++ webpack.common.js
@@ -32,6 +32,31 @@ module.exports = {
       },
     ],
   },
+
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: "~",
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+
   plugins: [
     new CleanWebpackPlugin(),
 
@@ -39,6 +64,8 @@ module.exports = {
       filename: "index.html",
       template: path.resolve(__dirname, "src/templates/index.html"),
     }),
+
+    new BundleAnalyzerPlugin(),
 
     new CopyWebpackPlugin({
       patterns: [
@@ -114,7 +141,5 @@ module.exports = {
         }),
       ],
     }),
-
-    new BundleAnalyzerPlugin(),
   ],
 };
